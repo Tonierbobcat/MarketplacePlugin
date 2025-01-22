bfa1cc2ed9e05814ccd28d27d1407190a63df5fd
Logan <tonierbobcat966@gmail.com>
2025-01-22 14:16:51 -0800

    Initial

---
+++ src/main/java/com/loficostudios/marketplacePlugin/MarketplacePlugin.java
@@ -0,0 +1,14 @@
+package com.loficostudios.marketplacePlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MarketplacePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
